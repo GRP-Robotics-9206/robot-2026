f0a6af0416f43828ef72bde39d6a4e68670885d1
Eclipse-5214 <elijahlipscomb55@gmail.com>
2026-01-19 11:25:34 -0500

    updaate libs

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "robot-2026";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "58f9c1d501a093eb1168c0111e87b26d037bb14b";
+  public static final String GIT_DATE = "2026-01-16 11:24:31 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-19 11:06:30 EST";
+  public static final long BUILD_UNIX_TIME = 1768838790710L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
